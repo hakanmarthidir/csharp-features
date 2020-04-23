34e575a3a4720d562976d2869784bf1d29a411e9
hakanmarthidir <hakan_hidir@hotmail.com>
2020-04-24 00:26:38 +0200

    Disposable ref structs sample

---
+++ Features_8/DisposableRefStructs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Features_8
+{
+
+    public class DisposableRefStructs
+    {
+
+        public DisposableRefStructs()
+        {
+            using var mystruct = new DisposableStruct();
+
+        }
+    }
+
+    //'DisposableStruct': ref structs cannot implement interfaces !!!
+
+    //public ref struct DisposableStruct : IDisposable
+    //{
+    //    public void Dispose()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+
+
+    // So ... it works
+    ref struct DisposableStruct
+    {
+        public void Dispose()
+        {
+            // no need to implementation
+        }
+    }
+
+}
